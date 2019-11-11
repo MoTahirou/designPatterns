1225b89d7ee22fe20e9d9a5ccdcb024256990fdf
Mo Tahirou <mahamadou.tahirou@gmail.com>
2019-11-10 20:31:00 -0600

    Fixing gradle

--- patternRunner/src/main/java/App.java
+++ patternRunner/src/main/java/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) {
 
-        SingletonImplementation single = new SingletonImplementation();
+      SingletonImplementation single = SingletonImplementation.getInstance();
+      System.out.println(single);
+
+      SingletonImplementation single2 = SingletonImplementation.getInstance();
+      System.out.println(single2);
+
+
     }
 }
--- singleton/src/test/groovy/SingletonImplementationSpec.groovy
+++ singleton/src/test/groovy/SingletonImplementationSpec.groovy
@@ -1,4 +1,13 @@
 import spock.lang.Specification
 
 class SingletonImplementationSpec extends  Specification{
+
+    void 'Our singleton is a true singleton'() {
+        given:
+        SingletonImplementation single1  = SingletonImplementation.getInstance()
+        SingletonImplementation single2  = SingletonImplementation.getInstance()
+
+        expect:
+        single1 == single2
+    }
 }
